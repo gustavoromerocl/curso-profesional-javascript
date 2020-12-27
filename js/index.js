5c6ef9ff752d3eccf31d0f37c3d56ff731f31906
Romero <gandresrp@gmail.com>
2020-12-27 16:42:09 -0300

    Conceptos basicos - Variables y constantes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo")
+console.log("Hola mundo") //Impresion de datos
+//prompt() //Recibe un dato mediante un input y lo imprime por consola
+
+//Variables: El valor cambia en tiempo de ejecución
+let nombre = "Gustavo";
+var apellido = "Romero";
+
+//Constante: El valor no cambia en tiempo de ejecución
+const pi = 3.1416;
