d7003fb9f0fa3e6166c80a391918bf5e97635db8
Romero <gandresrp@gmail.com>
2020-12-27 17:02:24 -0300

    Conceptos basicos - Booleanos

--- js/index.js
+++ js/index.js
@@ -16,3 +16,9 @@ console.log(Math.pow(10,2)) //Elevar un número al cuadrado
 console.log(Math.pow(10,3)) //Elevar un número a el cubo
 console.log(Math.round(6.5)) //Redondea el número que recibe
 console.log(Math.sqrt(4)) //Raíz cuadrada del número que recibe
+
+//Booleanos
+let booleano = new Boolean(1);
+console.log(booleano.toString());
+
+//Valores que retornan falso = undefined, NaN, null, 0, -0, "", false
