94a13468f20bfd4ebe301016533d6f6f7bd943d5
Romero <gandresrp@gmail.com>
2020-12-29 23:09:45 -0300

    Funciones y el scope - Que es el scope

--- js/funciones-y-el-scope.js
+++ js/funciones-y-el-scope.js
@@ -9,3 +9,22 @@ function cuadrado(numero){
 let cuadrado_de_dos = cuadrado(2);
 
 console.log(cuadrado_de_dos)
+
+//Qué es el scope (Alcance)
+
+var nombre = "Gustavo"; //Scope global
+
+function saludar(){
+    //Scope local
+    console.log("hola " + nombre );
+}
+
+saludar();
+
+function switch_nombres(){
+    var nombre = "Marcos"
+    console.log(nombre) //Scope local
+}
+
+switch_nombres() //scope local
+console.log(nombre) //Scope global
