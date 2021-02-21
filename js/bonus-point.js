f21591957364176ef2b84c7552d58080b652c19f
Romero <gandresrp@gmail.com>
2021-02-21 08:47:49 -0300

    Bonus Point - Spread Operator

---
+++ js/bonus-point.js
@@ -0,0 +1,41 @@
+/** SPREAD OPERATOR */
+
+let numeros = [2,3,5];
+
+let otro_arreglo = ['hola', 'mundo']
+
+function sumar(n1,n2,n3){
+  return n1+ n2 + n3;
+}
+
+//apply aplica el contexto y desonctuye el arreglo entregando elemento por elemento
+let resultado = sumar.apply(this, numeros);
+
+//Una forma más optima de pasar los argumentos seria con spread operator [1,2,3] === 1 , 2 , 3
+let resultado_2 = sumar(...numeros);
+
+console.log(resultado);
+
+//De esta forma es posible conbinar 2 arreglos
+console.log([...numeros, ...otro_arreglo])
+
+//Agregar un elmento al arreglo
+console.log([...numeros, 7])
+
+
+//También es posible combinar objetos
+let objeto = {
+  clave: 3
+}
+
+let objetoDos = {
+  otraclave: 4
+}
+
+let newObject = {
+  ...objeto,
+  ...objetoDos
+}
+console.log(objeto);
+console.log(objetoDos);
+console.log(newObject);
