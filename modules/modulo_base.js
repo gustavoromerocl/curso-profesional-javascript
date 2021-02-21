d2f6e85ad87149f7e9223a0abfd1b6b0e836a67b
Romero <gandresrp@gmail.com>
2021-02-21 14:36:48 -0300

    Modulos - Valores por defecto

--- modules/modulo_base.js
+++ modules/modulo_base.js
@@ -1,3 +1,6 @@
-import {name} from './modulo_dos.js';
+//import {name} from './modulo_dos.js';
 
+import def, {name} from './modulo_dos.js';
+
+console.log(def);
 console.log(name);
--- modules/modulo_dos.js
+++ modules/modulo_dos.js
@@ -1,1 +1,5 @@
 export const name = 'Tavo';
+
+let valorPorDefecto = 20;
+//Los modulos solo pueden tener un export default
+export default valorPorDefecto; 
