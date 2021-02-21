cd13121b4df6d5a65ec834beecad531be81c9a93
Romero <gandresrp@gmail.com>
2021-02-21 20:26:36 -0300

    Cadenas a fondo - Caracteres especiales

--- js/cadenas-a-fondo.js
+++ js/cadenas-a-fondo.js
@@ -1,4 +1,6 @@
-/**Cadenas a fondo */
+/**CADENAS A FONDO*/
+
+/** Como definir una cadena */
 
 let nombre = "Gustavo";
 console.log(nombre.length);
@@ -7,3 +9,12 @@ let name = new String("Gustavo");
 
 console.log(typeof nombre);
 console.log(typeof name);
+
+/** Caracteres especiales */
+
+
+//Escaping
+
+console.log("Prueba de \"caracteres\" ");
+
+// \n \t
