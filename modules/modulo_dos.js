2181b9f3d1d05555375efea64a43e2085737cfb6
Romero <gandresrp@gmail.com>
2021-02-21 15:36:45 -0300

    Modulos - ReadOnly imports

--- modules/modulo_dos.js
+++ modules/modulo_dos.js
@@ -1,5 +1,8 @@
 export const name = 'Tavo';
+export let edad = 30;
 
+//Los modulos son de categoria read only, por lo que solo pueden ser modificados dentro del mismo modulo.
+edad = 31
 let valorPorDefecto = 20;
 //Los modulos solo pueden tener un export default
 export default valorPorDefecto; 
