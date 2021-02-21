02615fb7486a160820a5c0bd1cecfeaa66d66c38
Romero <gandresrp@gmail.com>
2021-02-21 16:37:05 -0300

    Generadores e iteradores - Generadores

--- js/generadores-e-iteradores.js
+++ js/generadores-e-iteradores.js
@@ -1,5 +1,7 @@
 /**GENERADORES E ITERADORES */
 
+//ITERADORES 
+
 let iterador = {
   currentValue: 1,
   next(){
@@ -20,7 +22,7 @@ console.log(iterador.next());
 console.log(iterador.next());
 console.log(iterador.next());
 setTimeout(() => console.log(iterador.next()),5000);
-  */
+
 
 
 let item = iterador.next();
@@ -28,3 +30,31 @@ while (!item.done){
   console.log(item.value);
   item = iterador.next();
 }
+*/
+
+
+/*GENERADORES
+
+//Las funciones generadoras * retornan generadores de forma implicita
+function* counter(){
+  console.log('Estoy aquí');
+  yield 1;
+  console.log('Ahora estoy aquí');
+  yield 2;
+}
+*/
+
+//yield detiene la ejecución del ciclo for
+function* counter(){
+  for(var i = 1; i <= 5; i++){
+    yield i;
+  }
+}
+
+let generator = counter();
+
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
