a1424dd901a51e29e38035c1925f0e89b8bf0028
Romero <gandresrp@gmail.com>
2021-02-21 00:04:25 -0300

    Programacion asincrona - Promesas

--- js/programacion-asincrona.js
+++ js/programacion-asincrona.js
@@ -1,6 +1,10 @@
 //npm install request
 //node "nombre del archivo"
 
+/* PROGRAMACION ASÍNCRONA*/
+
+/* CALLBACKS 
+
 const request = require('request');
 
 //Un CallBack es una funcion que se pasa como parametro a otra función asíncrona, esta se ejecuta despues de completar la primera peticion (async)
@@ -9,3 +13,20 @@ request('http://google.com', function(){
 });
 
 console.log('Yo voy despues de la patición en el codigo');
+*/
+
+/** PROMESAS  */
+
+//fullfield: promesa complida
+//rejected: promesa no se cumple
+//pending: promesa no se ha cumplido
+//setled: promesa terminada
+
+const rp = require('request-promise');
+
+rp('http://google.com')
+  .then(function(html){
+    console.log('Termine la peticion de Google');
+  }).catch(function(e){
+    console.log(e);
+  });
