0e48c5499dd08f74f96df470527b4bdfe56e0ba6
Romero <gandresrp@gmail.com>
2021-02-22 12:28:26 -0300

    Expresiones regulares - Rangos en regex

--- js/expresiones-regulares.js
+++ js/expresiones-regulares.js
@@ -1,6 +1,6 @@
 /**Expresiones regulares */
 
-//  /Hola/
+/Hola/
 
 //Literales, especiales (meta caracteres)
 //Especiales: tipo caracter, las aserciones, agrupamientos rangos, cuantificadores, unicode property escape
@@ -23,7 +23,19 @@ console.log(texto.match(/\d+/g));
 
 /** Reemplazando patrones */
 
-//Si se agrega g a la expresion regular, realiza el cambio en todas las instancias de la cadena
+//Si se agrega g a la expresion regular, realiza el cambio en todas las instancias de la cadena, el mismo resultado se puede obtener usando el metodo replace all
 let nuevoTexto = texto.replace(/Gustavo/g,"Gustavo Romero");
 
 console.log(nuevoTexto);
+
+/**Rangos en Regex 
+
+/[abcd]/  Esta expresiones hace match con cualquiera de las letras del grupo {}
+/abcd/ Esta expresion solo hace match con el conjunto completo abcd
+
+*/
+
+console.log("abcd".match(/[abcd]/));
+console.log("abcd".match(/abcd/));
+console.log("b".match(/[a-d]/));
+console.log("3".match(/[0-5]/));
