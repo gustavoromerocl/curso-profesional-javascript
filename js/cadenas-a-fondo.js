f64b7c77ee86044cd5e11890e41728eaabdbc2ce
Romero <gandresrp@gmail.com>
2021-02-21 21:12:27 -0300

    Cadenas a fondo - subcadenas y caracteres

--- js/cadenas-a-fondo.js
+++ js/cadenas-a-fondo.js
@@ -54,3 +54,19 @@ let segundaPrueba = prueba.toUpperCase();
 
 console.log(prueba);
 console.log(segundaPrueba);
+
+/** Subcadenas y caracteres */
+
+console.log("hola"[0]);
+
+let cadena = "Hola Mundo";
+
+for(const caracter of cadena){console.log(caracter)};
+
+//Recorre y muestra la cadenas en las posiciones indicadas
+console.log(cadena.substring(0,2));
+console.log(cadena.slice(2,4));
+console.log(cadena.slice(-5));
+
+//https://stackoverflow.com/questions/2243824/what-is-the-difference-between-string-slice-and-string-substring#:~:text=The%20slice()%20method%20extracts,parts%20in%20a%20new%20string.&te
+
